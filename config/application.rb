7fbe97d59d36b51f41d80e7a41cc911cf1bef159
tomgdow <thomasgdowling@gmail.com>
2016-10-29 21:11:50 +0100

    add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -17,14 +17,22 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module Dowapi
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+	class Application < Rails::Application
+		# Settings in config/environments/* take precedence over those specified here.
+		# Application configuration should go into files in config/initializers
+		# -- all .rb files in that directory are automatically loaded.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
+		# Only loads a smaller set of middleware suitable for API only apps.
+		# Middleware like session, flash, cookies can be added back manually.
+
+		config.middleware.insert_before 0, Rack::Cors do
+			allow do
+				origins '*'
+				resource '*', :headers => :any, :methods => [:get, :post, :options]
+			end
+		end
+
+		# Skip views, helpers and assets when generating a new resource.
+		config.api_only = true
+	end
 end
