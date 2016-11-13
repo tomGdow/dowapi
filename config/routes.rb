886bb7fab8c98f98c153916f4efd50388559352e
tomgdow <thomasgdowling@gmail.com>
2016-11-13 20:37:17 +0000

    fix route redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :taoisigh
   resources :hurling_all_irelands
   resources :football_all_irelands
+
+	resources :taoisigh do
+		collection do
+			get 'fiannafail'
+			get 'finegael'
+		end
+	end
+
+	get '/fiannafail' => redirect('/taoisigh/fiannafail')
+	get '/finegael' => redirect('/taoisigh/finegael')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
